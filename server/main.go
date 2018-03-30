1c6c1164ebaf4bd256f4f03dcd800d61d8cf137b
Lazar Tsarev <lazar.tsarev@paysafe.com>
2018-03-30 13:14:56 +0300

    Add history slice, post and get endpoints implemented.

--- server/main.go
+++ server/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"net/http"
 	"encoding/json"
 	"fmt"
-	"net/http"
 )
+
+var history []string
 
 type testStruct struct {
 	Temperature string
 }
 
 func parseGhPost(rw http.ResponseWriter, request *http.Request) {
+	if request.Method == "GET" {
+		handleGet(rw, request)
+	} else if request.Method == "POST" {
+		handlePost(rw, request)
+	} else {
+		rw.WriteHeader(405)
+	}
+}
+
+func handlePost(rw http.ResponseWriter, request *http.Request) {
 	decoder := json.NewDecoder(request.Body)
 
 	var t testStruct
@@ -22,9 +34,19 @@ func parseGhPost(rw http.ResponseWriter, request *http.Request) {
 
 	// f, err := strconv.ParseFloat("3.1415", 64)
 	fmt.Println(t.Temperature)
+	history = append(history, t.Temperature)
+}
+
+func handleGet(rw http.ResponseWriter, request *http.Request) {
+	var encoder = json.NewEncoder(rw)
+	var err = encoder.Encode(history)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", parseGhPost)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/temperature/log", parseGhPost)
+	http.HandleFunc("/temperature/log", parseGhPost)
+	http.ListenAndServe(":80", nil)
 }
